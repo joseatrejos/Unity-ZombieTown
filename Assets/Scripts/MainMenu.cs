9c05e0d522e35c3e0030b116fd08bf0f1255f1eb
Ana Lucia Blanco <analuciablanco@outlook.com>
2020-05-13 19:37:39 -0700

    Main Menu minor changes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene(1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ContinueGame()
     {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
         
     }
 }
