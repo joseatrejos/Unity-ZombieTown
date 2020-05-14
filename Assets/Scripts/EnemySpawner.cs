34028ab71dec71049454317f1ffc06b808ab18c4
Maria Barraza <reikafer@gmail.com>
2020-05-13 17:38:29 -0700

    Dialog bugs fixed and Enemy pool, enemies disappear and appear randomly when you kill them

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    ObjectPooler objectPooler;
+
+    private void Start()
+    {
+        objectPooler = ObjectPooler.Instance;
+    }
+    void FixedUpdate()
+    {
+        objectPooler.SpawnFromPool("Enemy", this.transform.position);
+    }
+}
